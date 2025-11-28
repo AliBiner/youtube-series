150e589b7631e1cbc44ec735e5dcf8584069dba1
Ali Biner <alibiner.github@gmail.com>
2025-11-28 11:15:35 +0300

    ikinci ders

---
+++ jd/src/main/java/com/jd/ders2/App.java
@@ -0,0 +1,11 @@
+package com.jd.ders2;
+
+public class App {
+    public static void main(String[] args) {
+        String metin1 = "Merhaba Dünya!"; //variable x,y,z, metin1
+        String text = " Hoşgeldiniz!";
+
+
+        System.out.println(metin1 + text);
+    }
+}
