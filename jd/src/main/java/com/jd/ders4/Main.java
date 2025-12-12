bbdbacbe35b74425d915766cbeb476243f49b15a
Ali Biner <alibiner.github@gmail.com>
2025-12-12 15:19:35 +0300

    ders 4

---
+++ jd/src/main/java/com/jd/ders4/Main.java
@@ -0,0 +1,24 @@
+package com.jd.ders4;
+
+public class Main {
+    public static void main(String[] args) {
+        byte s1 = 0;
+        short s2 = 30_000;
+        int s3 = 100000000;
+        long s4 = 1_000_000_000;
+        float s5 = 32.32f;
+        double s6 = 64.64;
+        boolean choise = true;
+        char c1 = 'A';
+
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+        System.out.println(s4);
+        System.out.println(s5);
+        System.out.println(s6);
+        System.out.println(choise);
+        System.out.println(c1);
+
+    }
+}
