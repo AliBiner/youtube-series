05806f0c9d353938649b14b8c3305267d22e9c83
Ali Biner <alibiner.github@gmail.com>
2025-12-21 12:01:37 +0300

    ders8

---
+++ jd/src/main/java/com/jd/ders8/Main.java
@@ -0,0 +1,45 @@
+package com.jd.ders8;
+
+// Comparison Operators
+// ==
+// !=
+// <
+// >
+// <=
+// >=
+
+//Logical Operators
+// && - ve - bütün koşullar doğru ise bize true döner. herhangi bir koşul doğru değilse false döner.
+
+// || - veya - bütün durumlar veya herhagi bir durum doğru ise bize true döner. ama bütün koşullar yanlış ise false döner.
+
+// ! - değil
+
+public class Main {
+    public static void main(String[] args) {
+        
+        int age = 30;
+        int plakaKodu = 63;
+
+        //System.out.println(age == 29);
+
+        //System.out.println(age != 45);
+
+        //System.out.println(age < 30);
+
+        //System.out.println(age > 30);
+
+        //System.out.println(age >= 35);
+
+        //System.out.println( age <= 20);
+
+        //System.out.println(age <= 20 || plakaKodu == 63);
+
+        //System.out.println( age == 20 && plakaKodu == 60);
+
+        //System.out.println( age == 20 || plakaKodu == 60);
+
+        //System.out.println( !(age == 20 || plakaKodu == 60));
+
+    }
+}
