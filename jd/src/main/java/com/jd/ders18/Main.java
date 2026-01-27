f2a44362b5fd22460ef5516539d5a5504a2d5298
Ali Biner <alibiner.github@gmail.com>
2026-01-27 18:17:38 +0300

    ders18.1

--- jd/src/main/java/com/jd/ders18/Main.java
+++ jd/src/main/java/com/jd/ders18/Main.java
@@ -4,43 +4,47 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        //döngü - loop - for loop / while / do-while
-
+        run();
         // Kullanıcıda 4 sayı al. eğer sayı pozitifse sayıları topla.
-
-        Scanner scanner = new Scanner(System.in);
-
-        /* System.out.println("sayı 1'i giriniz: ");
-        int sayi1 = scanner.nextInt();
-
-        System.out.println("sayı 2'i giriniz: ");
-        int sayi2 = scanner.nextInt();
-
-        System.out.println("sayı 3'i giriniz: ");
-        int sayi3 = scanner.nextInt();
-
-        System.out.println("sayı 4'i giriniz: ");
-        int sayi4 = scanner.nextInt();
-
-        int toplam = sayi1 + sayi2 + sayi3 + sayi4; */
-
-        int toplam = 0;
-
-        for(int i = 0; i<4; i++){ // 0, 1,2,3,4
-
-            String mesaj = (i + 1) + ". sayıyı giriniz: ";
-        
-            System.out.println(mesaj);
-            int sayi = scanner.nextInt();
-            if(sayi>0){
-                toplam = toplam + sayi;
-            } 
-        }
-        
-        System.out.println("toplam değer: " + toplam);
-
     }
 
+    public static int sayiGetir(String mesaj){
+            Scanner scanner = new Scanner(System.in);
     
+            System.out.println(mesaj);
+            int sayi = scanner.nextInt();
 
+            return sayi;
+    }
+    
+    public static boolean sayiPozitifMi(int deneme){
+        if(deneme>0)
+            return true;
+        else
+            return false; 
+    }
+
+    public static int arttır(int artacakDeger, int artacakMiktar){
+        artacakDeger = artacakDeger + artacakMiktar;
+        return artacakDeger;
+    }
+
+    public static void run(){
+     
+
+        int a = 0;
+
+        for(int i = 0; i<4; i++){ // 0, 1,2,3,4
+            String message = (i + 1) + ". Sayiyi giriniz: ";
+            int b = sayiGetir(message);
+
+            if(sayiPozitifMi(b)){
+                a = arttır(a, b);
+            }
+        }
+
+        a = arttır(a, a);
+        
+        System.out.println("toplam değer: " + a); 
+    }
 }
