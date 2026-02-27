29863c19d4ab43a0c0e1b87af721258bfb985a4f
Ali Biner <alibiner.github@gmail.com>
2026-02-27 13:08:21 +0300

    ders 23 - class ve object farkı

---
+++ jd/src/main/java/com/jd/ders23/ClassApplication.java
@@ -0,0 +1,25 @@
+package com.jd.ders23;
+
+public class ClassApplication {
+    public static void main(String[] args) {
+        // String teacherName =  new String();
+        // String teacherLastName =  new String();
+        // int age = 25;
+
+        Teacher mahmut = new Teacher();
+        mahmut.name = "Mahmut";
+        mahmut.lastName = "deneme";
+        mahmut.age = 30;
+        //...
+
+        Teacher ayse = new Teacher();
+
+        ayse.name = "Ayse";
+        ayse.lastName = "test";
+        ayse.depermant = "Fen Bilimleri";
+        ayse.gender = "Kadın";
+        ayse.ID = "asdasd";
+        
+
+    }
+}
---
+++ jd/src/main/java/com/jd/ders23/Teacher.java
@@ -0,0 +1,11 @@
+package com.jd.ders23;
+
+public class Teacher {
+    String name;
+    String lastName;
+    String phone;
+    int age;
+    String depermant;
+    String ID;
+    String gender;
+}
