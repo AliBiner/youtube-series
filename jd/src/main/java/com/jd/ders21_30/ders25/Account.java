6d21789de7942f18cee2ffa44808b005305e7a08
Ali Biner <alibiner.github@gmail.com>
2026-03-06 03:06:39 +0300

    ders25 - encapsulation - bank app - Encapsulation uygulama - refactor

--- jd/src/main/java/com/jd/ders21_30/ders25/Account.java
+++ jd/src/main/java/com/jd/ders21_30/ders25/Account.java
@@ -3,15 +3,22 @@ package com.jd.ders21_30.ders25;
 import java.util.UUID;
 
 public class Account {
-    private String number; //default null
+    private final String number; //default null
     private String ownerName; //default null
     private double balance; //default 0.0
     private boolean isActive; //default false
 
+    private final static int OWNER_NAME_MIN_LENGTH = 6;
+    private final static String ACCOUNT_MUST_BE_ACTIVE = "Hesap Kapalı! İşlem yapamazsınız!";
+    private final static String AMOUNT_MUST_POZITIVE =  "Miktar pozitif olmalı!";
+    private final static String YETERSIZ_BALANCE = "Yetersiz bakiye!";
+
 
     //Constructor Method
     public Account(String ownerName){
-        setOwnerName(ownerName);
+        if(checkOwnerName(ownerName)){
+            this.ownerName = ownerName;
+        }
         this.number = UUID.randomUUID().toString();
         this.balance = 0.00;
         this.isActive = true;
@@ -20,15 +27,6 @@ public class Account {
     //getters
     public String getNumber() {
         return number;
-    }
-
-    public void setOwnerName(String ownerName){
-        if(ownerName == null)
-            System.out.println("Hesap Sahip Adı Null olamaz!");
-        else if(ownerName.length()<6)
-            System.out.println("Hesap Sahip Adı 6 krakterden az olamaz!");
-        else
-            this.ownerName = ownerName;
     }
 
     public String getOwnerName() {
@@ -43,13 +41,31 @@ public class Account {
         return isActive;
     }
 
+    public void setOwnerName(String ownerName){
+        if(checkOwnerName(ownerName)){
+            this.ownerName = ownerName;
+        }
+    }
+
+    private boolean checkOwnerName(String ownerName){
+        if(ownerName == null){
+            System.out.println("Hesap Sahip Adı Null olamaz!");
+            return false;
+        }
+        else if(ownerName.length()<OWNER_NAME_MIN_LENGTH){ 
+            System.out.println("Hesap Sahip Adı 6 krakterden az olamaz!");
+            return false;
+        }
+        return true;
+    }
+
     //domain logic methods
     public void depositByAmount(double amount){
         if(isActive == false){
-            System.out.println("Hesap Kapalı!");
+            System.out.println(ACCOUNT_MUST_BE_ACTIVE);
         }
         else if(amount<0){
-            System.out.println("Miktar pozitif olmalı!");
+            System.out.println(AMOUNT_MUST_POZITIVE);
         }else{
             this.balance = this.balance + amount;
             System.out.println(amount + " TL yatırıldı. Güncel Bakiye: " + this.balance);
@@ -58,12 +74,12 @@ public class Account {
 
     public void withdrawByAmount(double amount){
         if(isActive == false){
-            System.out.println("Hesap Kapalı!");
+            System.out.println(ACCOUNT_MUST_BE_ACTIVE);
         }
         else if(amount<0){
-            System.out.println("Miktar pozitif olmalı!");
+            System.out.println(AMOUNT_MUST_POZITIVE);
         }else if(amount > this.balance){
-            System.out.println("Yetersiz bakiye!");
+            System.out.println(YETERSIZ_BALANCE);
         }else{
             this.balance = this.balance - amount;
             System.out.println(amount + " TL çekildi. Güncel Bakiye: " + this.balance);
