a90d7cdea2828f25fc4f08ba8a5e283bfd7a47d8
Ali Biner <alibiner.github@gmail.com>
2026-05-01 14:51:59 +0300

    ders 28 - object class nedir?

--- jd/src/main/java/com/jd/ders21_30/ders26/vehicle/Car.java
+++ jd/src/main/java/com/jd/ders21_30/ders26/vehicle/Car.java
@@ -1,14 +1,12 @@
 package com.jd.ders21_30.ders26.vehicle;
 
-public class Car extends MotorizedVehicle  {
+public class Car extends MotorizedVehicle {
     private int numDoors;
     private boolean isAutomatic;
 
     public Car(String brand, String model, double rentalPrice, int engineCC, String fuelType) {
         super(brand, model, rentalPrice, engineCC, fuelType);
     }
-    
-    
 
     @Override
     public String getInfo() {
@@ -16,18 +14,49 @@ public class Car extends MotorizedVehicle  {
                 + this.getRentalPrice() + " engineCC: " + this.getEngineCC() + " fuelType: " + this.getFuelType();
     }
 
-    
-
     @Override
     public double calculateCost(int days) {
-        if (this.numDoors<=0) {
+        if (this.numDoors <= 0) {
             return super.calculateCost(days);
-        }else{
+        } else {
             return days * this.getRentalPrice() * this.numDoors;
         }
     }
 
+    
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + numDoors;
+        result = prime * result + (isAutomatic ? 1231 : 1237);
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Car other = (Car) obj;
+        if (numDoors != other.numDoors)
+            return false;
+        if (isAutomatic != other.isAutomatic)
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Car [numDoors=" + numDoors + ", isAutomatic=" + isAutomatic + ", getInfo()=" + getInfo()
+                + ", getBrand()=" + getBrand() + ", getModel()=" + getModel() + ", getEngineCC()=" + getEngineCC()
+                + ", getRentalPrice()=" + getRentalPrice() + ", getNumDoors()=" + getNumDoors() + ", getFuelType()="
+                + getFuelType() + ", isAutomatic()=" + isAutomatic() + "]";
+    }
 
     public int getNumDoors() {
         return numDoors;
---
+++ jd/src/main/java/com/jd/ders21_30/ders28/Main.java
@@ -0,0 +1,10 @@
+package com.jd.ders21_30.ders28;
+
+import com.jd.ders21_30.ders26.vehicle.Car;
+
+public class Main {
+    public static void main(String[] args) {
+        Car car = new Car(null, null, 0, 0, null);
+        System.out.println(car);
+    }
+}
